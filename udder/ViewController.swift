4df6aa21683852edd2befddf3e41ba261f08ef01
Jinal <jinal@artixun.com>
2020-07-12 19:17:23 +0530

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Artixun on 7/12/20.
+//  Copyright © 2020 Pk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
